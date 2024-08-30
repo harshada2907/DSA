0708c8c354d325a7eab49ed32aaace1be759aff6
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-08-30 21:38:03 +0530

    Update p9.py

---
+++ set-1/p9.py
@@ -0,0 +1,10 @@
+#Write a program to find Area of a circle
+import math
+def area(radius):
+  return math.pi*radius*radius
+radius=float(input("Enter the radius of the circle:"))
+result=area(radius)
+print("The area of the circle is:",rasult)
+
+  
+  
