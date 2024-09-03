4dd5b32c2806bd3a5fe559db6bde86c594c66f97
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-03 18:21:34 +0530

    Update p17.py

--- set-1/p17.py
+++ set-1/p17.py
@@ -1,2 +1,18 @@
 #Write a program to identify if the number is Strong number or not
+import math
 
+def is_strong_number(num):
+    digits = str(num)
+    sum_of_factorials = 0
+    for digit in digits:
+        sum_of_factorials += math.factorial(int(digit))
+    return sum_of_factorials == num
+
+number = int(input("Enter a number:"))
+
+if is_strong_number(number):
+    print(f"{number} is a strong number.")
+else:
+    print(f"{number} is not a strong number.")
+
+  
