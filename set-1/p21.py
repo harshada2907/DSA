2b20cc55756b186e71231ad533dce7706bf5c805
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-04 19:40:40 +0530

    Update p21.py

--- set-1/p21.py
+++ set-1/p21.py
@@ -1,1 +1,6 @@
-
+#Write a program to find Power of a number
+import math
+base=int(input("Enter the base number:"))
+exponent=int(input("Enter the exponent:"))
+result=pow(base,exponent)
+print(f"{base} raised to the power of{exponent} is {result}")
