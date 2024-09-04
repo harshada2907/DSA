5c4ef496f7ecdca7aac8c1d802a7007ab991403d
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-04 19:48:39 +0530

    Update p19.py

--- set-1/p19.py
+++ set-1/p19.py
@@ -1,1 +1,12 @@
-
+#Write a program to find the Factors of a number
+def find_factors(n):
+  factors=[]
+  for i in range(1,math.isqrt(n)+1):
+    if n % i==0:
+      factors.append(i)
+      if i!=n//i:
+        factors.append(n//i)
+  return sorted(factors)
+number=int(input("Enter a number:"))
+factors=find_factors(number)
+print(f"The factors of{number} are:{factors}")
