e99899042e40ce1de4e6788e1ebb7a49fd79a6dd
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-06 10:54:31 +0530

    Update p24.py

--- set-1/p24.py
+++ set-1/p24.py
@@ -1,1 +1,7 @@
+#Write a program to find GCD of two numbers
+import math
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number"))
+gcd=math.gcd(num1,num2)
+print(f"The GCD of {num1} and {num2} is:{gcd}")
 
