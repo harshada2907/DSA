df8bf94633cafc16bf9584331ca7fe18b5bfde99
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-08 19:58:11 +0530

    Update p2.py

--- set-2/p2.py
+++ set-2/p2.py
@@ -2,20 +2,27 @@
 #An Armstrong number (or Narcissistic number) is a number that is equal to the sum of its own digits raised to the power of the number of digits.
 #153 is na Armstrong number because 1^3+5^3+3^3=153
 
-39474 is alos an Armstrong number because 9^4+4^4+7^4+4^4=9474
+#39474 is alos an Armstrong number because 9^4+4^4+7^4+4^4=9474
 
 def is_armstrong(num):
-  num_of_digits=len(str(num))
-  sum_of_digits=sum(int(digit)**num_of_digits for digit in str(num))
-  return num==sum_of_digits
-def print_armstrong_numbers(Start,end):
-  print(f"Armstrong numbers between {start} and {end} are:")
-  for num in range(Start,end+1):
-    if is_armstrong(num):
-      print(num,end=" ")
-  print()
-start=int(input("Enter the starting number of the range:"))
-end=int(input("Enter the ending number of the range:"))
-print_armstrong_numbers(start,end)
   
+    num_of_digits = len(str(num))
+    
+   
+    sum_of_digits = sum(int(digit) ** num_of_digits for digit in str(num))
+    
   
+    return num == sum_of_digits
+
+def print_armstrong_numbers(start, end):
+    print(f"Armstrong numbers between {start} and {end} are:")
+    for num in range(start, end + 1):
+        if is_armstrong(num):
+            print(num, end=" ")
+    print()
+
+
+start = int(input("Enter the starting number of the range: "))
+end = int(input("Enter the ending number of the range: "))
+
+print_armstrong_numbers(start, end)
