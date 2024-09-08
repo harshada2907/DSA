30644d5875a439716d3d896d59891faafa57dbbe
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-08 19:44:12 +0530

    Update p2.py

--- set-2/p2.py
+++ set-2/p2.py
@@ -1,1 +1,13 @@
 #Write a program to print Armstrong numbers between two intervals
+#An Armstrong number (or Narcissistic number) is a number that is equal to the sum of its own digits raised to the power of the number of digits.
+#153 is na Armstrong number because 1^3+5^3+3^3=153
+
+39474 is alos an Armstrong number because 9^4+4^4+7^4+4^4=9474
+
+def is_armstrong(num):
+  num_of_digits=len(str(num))
+  sum_of_digits=sum(int(digit)**num_of_digits for digit in str(num))
+  return num==sum_of_digits
+def print_armstrong_numbers(Start,end):
+  
+  
