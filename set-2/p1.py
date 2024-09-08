719256fb697d89b2944be8d7086b56224ab0b2bf
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-08 19:36:16 +0530

    Update p1.py

--- set-2/p1.py
+++ set-2/p1.py
@@ -1,1 +1,17 @@
 #Write a program to print prime numbers in a given range
+def is_prime(num):
+  if num<2:
+    return False
+  for i in range(2,int(num**0.5)+1):
+    if num %i==0:
+      return False
+  return True
+def print_prime_number(Start,end):
+  print(f"Prime numbers between {start} and {end} are:")
+  for num in range(start,end+1):
+     if is_prime(num):
+       print(num,end="")
+  print()
+start=int(input("Enter the starting number of the range:"))
+end=int(input("Enter the ending number of the range:"))
+
