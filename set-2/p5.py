8587eb001d2f9a43135b99e919a3e2d616f473a0
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-10 00:05:00 +0530

    Update p5.py

--- set-2/p5.py
+++ set-2/p5.py
@@ -1,1 +1,9 @@
 #Write a program to print Pyramid pattern using stars
+def pyramid_pattern(rows):
+  for i in range(1,rows+1):
+    print(""*(rows-i),end="")
+    print("*" *i)
+num_rows=int("Enter the number of rows:"))
+pyramid_pattern(num_rows)
+    
+    
