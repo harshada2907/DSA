472514959b67cb5aebf190801108761b9efcf025
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-10 00:01:52 +0530

    Update p4.py

--- set-2/p4.py
+++ set-2/p4.py
@@ -1,1 +1,9 @@
 #Write a program to Replace all 0’s with 1 in a given integer
+def replace_zeros_with_ones(number):
+  number_str=str(number)
+  replaced_str=number_str.replace('0','1')
+  replaced_number=int(replaced_str)
+  return replaced_number
+input_number=int(input("Enter an integer:"))
+output_number=replace_zeros_with_ones(input_number)
+print(f"Number after replacing all 0's with 1's :{output_number}")
