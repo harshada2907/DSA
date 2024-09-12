2f220ca4fdf7b5d8c43a1809cedec7365766ce1b
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-12 13:49:56 +0530

    Update p6.py

--- set-2/p6.py
+++ set-2/p6.py
@@ -1,1 +1,11 @@
+#Write a program to print Pyramid pattern using numbers
 
+def print_pyramid(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i), end="")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+n = 5
+print_pyramid(n)
