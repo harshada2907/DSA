d79567a85af4cd4ab2f7f12768dbc368d76c0780
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-13 00:17:12 +0530

    Update p7.py

--- set-2/p7.py
+++ set-2/p7.py
@@ -1,9 +1,11 @@
-#Write a program to print Pyramid pattern using numbers
-def print_pyramid(n):
+#Write a program to print Palindromic pyramid pattern printing..
+def print_palindromic_pyramid(n):
   for i in range(1,n+1):
     print(''*(n-i),end='')
-    for j in range(1,i+1):
-      print(j,end='')
-    print()
+  for j in range(1,i+1):
+    print(j,end='')
+  for j in range(i-1,0,-1):
+    print(j,end='')
 rows=5
-print_pyramid(rows)
+print_palindromic_pyramid(rows)
+    
