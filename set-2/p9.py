a3e9840ad741c4f3217cabed35a3048dfbf0e636
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-14 00:08:23 +0530

    Update p9.py

--- set-2/p9.py
+++ set-2/p9.py
@@ -1,1 +1,21 @@
 #Write a program to find the Quadrants in which coordinates lie
+def find_quadrant(x, y):
+    if x > 0 and y > 0:
+        return "Quadrant I"
+    elif x < 0 and y > 0:
+        return "Quadrant II"
+    elif x < 0 and y < 0:
+        return "Quadrant III"
+    elif x > 0 and y < 0:
+        return "Quadrant IV"
+    elif x == 0 and y != 0:
+        return "On Y-axis"
+    elif y == 0 and x != 0:
+        return "On X-axis"
+    else:
+        return "Origin"
+
+x = float(input("Enter x-coordinate: "))
+y = float(input("Enter y-coordinate: "))
+
+print(find_quadrant(x, y))
