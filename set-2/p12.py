f18fbf3d0d4e4bcab4ca8ca4da3a163c35e3872d
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-15 13:01:05 +0530

    Update p12.py

--- set-2/p12.py
+++ set-2/p12.py
@@ -1,1 +1,15 @@
+#12. Write a program to find Permutations in which n people can occupy r seats in a classroom.
+import math
 
+def find_permutations(n, r):
+    if r > n:
+        return "Number of seats can't be more than the number of people."
+    
+    permutations = math.factorial(n) // math.factorial(n - r)
+    return permutations
+
+n = int(input("Enter the number of people (n): "))
+r = int(input("Enter the number of seats (r): "))
+
+result = find_permutations(n, r)
+print(f"The number of ways {n} people can occupy {r} seats is: {result}")
