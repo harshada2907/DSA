e9e83fc6636c08548426dfe4fc0f4b55397a7fc8
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-16 07:37:30 +0530

    Update p14.py

--- set-2/p14.py
+++ set-2/p14.py
@@ -1,1 +1,12 @@
-
+#Write a program for Decimal to binary conversion
+def decimal_to_binary(decimal):
+  if decimal==0:
+    return "0"
+  binary=""
+  while deciaml>0:
+    remainder=decimal%2
+    binary=str(remainder)+binary
+    decimal=decimal//2
+  return binary
+decimal_number=int(input("Enter a decimal number:"))
+binary_number=decimal_to_binary(decimal_number)
