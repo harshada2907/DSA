87b80742e2308e144f0c4ebe5d7595c01cdd9ff9
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-16 07:33:22 +0530

    Update p13.py

--- set-2/p13.py
+++ set-2/p13.py
@@ -1,1 +1,10 @@
-
+# Write a program for Binary to decimal conversion
+def binary_to_decimal(binary):
+  decimal=0
+  binary=str(binary)
+  for digit in binary:
+    decimal=decimal*2+int(digit)
+  return decimal
+binary_number=input("Enter a binary number:")
+decimal_number=binary_to_decimal(binary_number)
+print(f"The decimal equivalent of binary(binary_number} is {decimal_number}")
