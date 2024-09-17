527ad464c4385eee3ece4524eea2c901e996606d
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-17 11:00:12 +0530

    Update p17.py

--- set-2/p17.py
+++ set-2/p17.py
@@ -1,1 +1,8 @@
-
+#Write a program for Octal to binary conversion
+def octal_to_binary(octal):
+  decimal=int(octal,8)
+  binary=bin(decimal)
+  return binary[2:]
+octal_input=input("Enter an octal number:")
+binary_output=octal_to_binary(octal_input)
+print(f"The binary equivalent of octal {octal_input} is {binary_output}")
