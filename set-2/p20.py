594d3332fae5df3426253fbf57e54020477733ae
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-19 07:34:11 +0530

    Update p20.py

--- set-2/p20.py
+++ set-2/p20.py
@@ -1,1 +1,21 @@
+#Write a program to find number of integers which has exactly 9 divisors
+import math
+
+def count_numbers_with_nine_divisors(limit):
+    count = 0
+    for i in range(1, limit + 1):
+        divisors = 0
+        for j in range(1, int(math.sqrt(i)) + 1):
+            if i % j == 0:
+                if j * j == i:
+                    divisors += 1
+                else:
+                    divisors += 2
+        if divisors == 9:
+            count += 1
+    return count
+
+limit = 100000
+result = count_numbers_with_nine_divisors(limit)
+print(result)
 
