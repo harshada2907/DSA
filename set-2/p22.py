f82647f2f7af09de639912a3d13736ba644c1878
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-20 08:41:19 +0530

    Update p22.py

--- set-2/p22.py
+++ set-2/p22.py
@@ -1,1 +1,9 @@
+#Write a program to print Solid and hollow rectangle star pattern
+def solid_rectangle(rows,cols):
+  for i in range(rows):
+    print("*" *cols)
+rows=int(input("Enter the number of rows:"))
+cols=int(input("Enter the number of columns:"))
+print("Solid Rectangle:")
+solid_rectnagle(rows,cols)
 
