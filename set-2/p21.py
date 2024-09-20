4034fbd9ad1c425b20d76e7c1a826f56d83033b3
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-20 08:36:06 +0530

    Update p21.py

--- set-2/p21.py
+++ set-2/p21.py
@@ -1,2 +1,14 @@
 #Write a program to find roots of a quadratic equation
+import cmath
+def find_roots(a,b,c):
+  discriminant=(b**2)-(4*a*c)
+  root1=(-b+cmath.sqrt(discriminant))/(2*a)
+  root2=(-b-cmath.sqrt(discriminant))/(2*a)
+  return root1,root2
+a=float(input("Enter coefficient a:"))
+b=float(input("Enter coefficient b:"))
+c=float(input("Enter coefficient c:"))
+roots=find_roots(a,b,c)
+print(f"The roots of the quadratic equation are:{roots[0]} and {roots[1]}")
 
+  
