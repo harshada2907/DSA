dcba3788a2200ef98bd0cce54360e62067602860
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-21 00:08:09 +0530

    Update p24.py

--- set-2/p24.py
+++ set-2/p24.py
@@ -1,1 +1,14 @@
+#Write a program to print Diamond pattern printing using numbers.
+def print_diamond(n):
+    for i in range(1, n + 1):
+        print(' ' * (n - i), end='')
+        print(' '.join(str(i) for _ in range(i)))
+
+    for i in range(n - 1, 0, -1):
+        print(' ' * (n - i), end='')
+        print(' '.join(str(i) for _ in range(i)))
+
+n = int(input("Enter the number of rows: "))
+print_diamond(n)
+
 
