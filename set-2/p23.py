fa371cef4bf80b22b9fac0b263fa8fd4ccdd056c
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-21 00:06:40 +0530

    Update p23.py

--- set-2/p23.py
+++ set-2/p23.py
@@ -1,1 +1,12 @@
+#Write a program to print Diamond pattern printing using stars
+def print_diamond(n):
+    for i in range(n):
+        print(' ' * (n - i - 1), end='')
+        print('*' * (2 * i + 1))
 
+    for i in range(n - 2, -1, -1):
+        print(' ' * (n - i - 1), end='')
+        print('*' * (2 * i + 1))
+
+n = int(input("Enter the number of rows: "))
+print_diamond(n)
