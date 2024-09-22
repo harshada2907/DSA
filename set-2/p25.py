350c1c16f8ff84dad52211bc6ce048d3697fb205
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-22 14:56:19 +0530

    Update p25.py

--- set-2/p25.py
+++ set-2/p25.py
@@ -1,1 +1,10 @@
-
+#Write a program to print Floyd’s triangle
+def floyd_triangle(rows):
+  num=1
+  for i in range(1,rows+1):
+    for j in range(1,i+1):
+      print(num,end="")
+      num+=1
+     print()
+n=int(input("Enter the number of rows:"))
+floyd_triangle(n)
