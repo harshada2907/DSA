d8544bb32f56fa16a91c8da9e0ad807e16745642
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-22 15:01:52 +0530

    Update p26.py

--- set-2/p26.py
+++ set-2/p26.py
@@ -1,1 +1,20 @@
-
+#Write a program to print Pascal triangle
+def print_pascals_triangle(n):
+  for i in range(n):
+    print("" *(n-i),end="")
+    value=1
+    for j in range(i+1):
+      print(value,end="")
+      value=value*(i-j)//(j+1)
+    print()
+n=int(input("Enter the number of rows:"))
+print_pascals_traingle(n)
+    
+    
+    
+    
+    
+    
+    
+    
+    
