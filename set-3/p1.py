d4c82c7d749c07f7364a1404d392ee2b74124937
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-23 09:57:20 +0530

    Update p1.py

--- set-3/p1.py
+++ set-3/p1.py
@@ -1,1 +1,16 @@
+# Write a Program to copy a string
 
+def copy_string(original):
+    copied = original  # Copying the string
+    return copied
+
+
+original_string = "Hello, World!"
+copied_string = copy_string(original_string)
+
+print("Original String:", original_string)
+print("Copied String:", copied_string)
+
+
+  
+  
