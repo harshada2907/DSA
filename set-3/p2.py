86586203168ef5b373e531d8c697bbe7b7e6173a
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-23 10:00:52 +0530

    Update p2.py

--- set-3/p2.py
+++ set-3/p2.py
@@ -1,1 +1,10 @@
-
+#Write a Program to reverse a string.
+def reverse_string(original):
+  reversed_string=original[::-1]
+  return reversed_string
+original_string="Hello,World!"
+reversed_string=reverse_string(original_string)
+print("Original String:",original_string)
+print("Reversed String:"reversed_string)
+  
+  
