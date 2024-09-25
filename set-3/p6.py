c86a3665f8135bb9a220f6ff31b06cdee5ec1348
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-25 06:49:22 +0530

    Update p6.py

--- set-3/p6.py
+++ set-3/p6.py
@@ -1,1 +1,8 @@
-
+#Write a Program to compare two strings.
+str1=input("Enter first string")
+str2=input("Enter second string")
+if str1=str2:
+  print("String are same")
+else:
+  print("String are different")
+  
