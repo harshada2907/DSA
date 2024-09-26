008ff78fb6f57f7bbaa7aad6e11ad89e02efc5d6
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-26 11:28:25 +0530

    Update p7.py

--- set-3/p7.py
+++ set-3/p7.py
@@ -1,1 +1,9 @@
-
+#Write a Program to print Length of the string without using strlen() function
+def string_length(s):
+  length=0
+  for char in s:
+    length+=1
+  return length
+user_string=input("Enter a string:")
+length=string_length(user_string)
+print(f"Length of the string:{length}")
