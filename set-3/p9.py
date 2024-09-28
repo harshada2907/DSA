55a8428961ec7bf35df5158977e99486d0d605f9
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-28 12:12:53 +0530

    Update p9.py

--- set-3/p9.py
+++ set-3/p9.py
@@ -1,1 +1,8 @@
-
+#Write a Program to Remove vowels from a string
+def remove_vowels(input_string):
+  vowels="aeiouAEIOU"
+  result=''.join(char for char in input_string if char not in vowels)
+  return result
+input_string=input("Enter a string:")
+output_string=remove_vowels(input_string)
+print("string after removing vowels:",output_string)
