45069b9dd5d512d0289ef54a249611ad0036cfae
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-28 11:56:05 +0530

    Update p8.py

--- set-3/p8.py
+++ set-3/p8.py
@@ -1,2 +1,16 @@
 #Write a Program to Toggle each character in a string
+def toggle_string(s):
+   toggled=""
+   for char in s:
+     if char.isupper():
+       toggled+=char.lower()
+     elif char.islower():
+       toggled+=char.upper()
+     else:
+       toggled+=char
+    return toggled
+input_string="Hello World!"
+toggled_string=toggle_string(input_string)
+print("Original String:",input_string)
+print("Toggled String:",toggled_string)
 
