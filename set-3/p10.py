569af5fae8da86f3e488671524e367ceb10546d9
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-09-29 09:09:20 +0530

    Update p10.py

--- set-3/p10.py
+++ set-3/p10.py
@@ -1,1 +1,11 @@
+#Write a Program to check if String is a palindrome or not
+def is_palindrome(s):
+    cleaned_string = ''.join(char.lower() for char in s if char.isalnum())
+    return cleaned_string == cleaned_string[::-1]
+
+input_string = "A man, a plan, a canal, Panama"
+if is_palindrome(input_string):
+    print(f'"{input_string}" is a palindrome')
+else:
+    print(f'"{input_string}" is not a palindrome')
 
