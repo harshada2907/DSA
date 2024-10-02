c9bc0ba520a64086c075ed63b3953495c18931f0
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-02 09:23:54 +0530

    Update p12.py

--- set-3/p12.py
+++ set-3/p12.py
@@ -1,1 +1,12 @@
+#12.Write a Program to Remove brackets from an algebraic expression
+def remove_brackets(expression):
+    expression = expression.replace("(", "")
+    expression = expression.replace(")", "")
+    expression = expression.replace("{", "")
+    expression = expression.replace("}", "")
+    expression = expression.replace("[", "")
+    expression = expression.replace("]", "")
+    return expression
 
+algebraic_expression = input("Enter the algebraic expression: ")
+print("Expression without brackets:", remove_brackets(algebraic_expression))
