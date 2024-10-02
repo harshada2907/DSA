df7327f623fd241219308010a15ce64ace84814b
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-03 00:30:47 +0530

    Update p13.py

--- set-3/p13.py
+++ set-3/p13.py
@@ -1,1 +1,8 @@
 
+def remove_non_alphabets(input_string):
+    filtered_string = ''.join(char for char in input_string if char.isalpha())
+    return filtered_string
+
+input_str = "Hello! Welcome to 2024: Let's code."
+result = remove_non_alphabets(input_str)
+print(result)  # Output: HelloWelcometLetscode
