4c4671ff9b2b4f65dca84197f9e76cd2ed026acb
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-04 18:20:37 +0530

    Update p15.py

--- set-3/p15.py
+++ set-3/p15.py
@@ -1,1 +1,7 @@
-
+#13.Write a Program to Remove characters in a string except alphabets
+def remove_non_alphabets(input_string):
+  return ''.join([char for char in input_string if char.isalpha()])
+string_with_characters="Hello,World! 123 @#"
+string_only_alphabets=remove_non_alphabets(string_with_characters)
+print("Original String:",string_with_characters)
+print("String with only alphabest:"String_only_alphabets)
