350043890e78615028d409e2bd437cd6fac2928f
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-04 18:15:42 +0530

    Update p14.py

--- set-3/p14.py
+++ set-3/p14.py
@@ -1,1 +1,7 @@
-
+#14.Write a Program to Remove spaces from a string
+def remove_spaces(input_string):
+  return input_string.replace(" ","")
+string_with_spaces="Hello World! How are you?"
+string_without_spaces=remove_spaces(string_with_spaces)
+print("Original String:",string_with_spaces)
+print("String without spaces:",string_without_spaces)
