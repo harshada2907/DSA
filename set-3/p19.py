7c9e5ba102a64f2cf29a6d5f6c4345b3450a1aba
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 18:54:18 +0530

    Update p19.py

--- set-3/p19.py
+++ set-3/p19.py
@@ -1,1 +1,17 @@
+#18.Write a Program to print Non-repeating characters in a string
+def non_repeating_characters(input_string):
+    frequency = {}
+    
+    for char in input_string:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    
+    result = [char for char in input_string if frequency[char] == 1]
+    
+    return ''.join(result)
 
+input_string = "hello world"
+result = non_repeating_characters(input_string)
+print("Non-repeating characters:", result)
