00c6e9de9f2db8bf7d92b3c9d050268c18f04ed6
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 19:02:10 +0530

    Update p21.py

--- set-3/p21.py
+++ set-3/p21.py
@@ -1,1 +1,10 @@
+#20.Write a Program to Replace substring in a string
+def replace_substring(original_string, old_substring, new_substring):
+    return original_string.replace(old_substring, new_substring)
 
+original_string = "I love programming"
+old_substring = "programming"
+new_substring = "Python"
+result = replace_substring(original_string, old_substring, new_substring)
+
+print("Modified string:", result)
