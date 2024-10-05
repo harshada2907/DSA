226f60a87fdd7275c5e0cd216ffc9200fef37998
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 18:52:14 +0530

    Update p17.py

--- set-3/p17.py
+++ set-3/p17.py
@@ -1,1 +1,17 @@
+#16.Write a Program to Capitalize the first and last letter of each word of a string
+def capitalize_first_last_letters(input_string):
+    words = input_string.split()
+    result = []
+    
+    for word in words:
+        if len(word) > 1:
+            word = word[0].upper() + word[1:-1] + word[-1].upper()
+        else:
+            word = word.upper()
+        result.append(word)
+    
+    return ' '.join(result)
 
+input_string = "hello world python is fun"
+result = capitalize_first_last_letters(input_string)
+print("Result:", result)
