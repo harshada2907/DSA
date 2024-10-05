d04c0c084a16551867e81acd9a56a82bc54b5213
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 18:50:50 +0530

    Update p16.py

--- set-3/p16.py
+++ set-3/p16.py
@@ -1,1 +1,21 @@
+#15.Write a Program to Count the sum of numbers in a string
+def sum_of_numbers_in_string(input_string):
+    total_sum = 0
+    current_number = ''
+    
+    for char in input_string:
+        if char.isdigit():
+            current_number += char
+        else:
+            if current_number != '':
+                total_sum += int(current_number)
+                current_number = ''
+    
+    if current_number != '':
+        total_sum += int(current_number)
+    
+    return total_sum
 
+input_string = "The price is 40 dollars and the tax is 5"
+result = sum_of_numbers_in_string(input_string)
+print("The sum of numbers in the string is:", result)
