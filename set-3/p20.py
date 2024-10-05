9133054435d67b476976bc8945534610bba91b13
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 19:01:15 +0530

    Update p20.py

--- set-3/p20.py
+++ set-3/p20.py
@@ -1,1 +1,14 @@
+#19.Write a Program to check if two strings are Anagram or not
+def are_anagrams(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+str1 = "listen"
+str2 = "silent"
+result = are_anagrams(str1, str2)
+
+if result:
+    print(f"{str1} and {str2} are anagrams")
+else:
+    print(f"{str1} and {str2} are not anagrams")
+
 
