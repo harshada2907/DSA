4f5981e32bcce0fdffa4002c5e88224d791c26bf
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-05 18:53:17 +0530

    Update p18.py

--- set-3/p18.py
+++ set-3/p18.py
@@ -1,1 +1,15 @@
+#17.Write a Program to calculate the Frequency of characters in a string
+def character_frequency(input_string):
+    frequency = {}
+    
+    for char in input_string:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    
+    return frequency
 
+input_string = "hello world"
+result = character_frequency(input_string)
+print("Character frequency:", result)
