d6283d0cdedc72e7703ec1b86ef0d9391e2a4c20
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-08 06:54:35 +0530

    Update p3.py

--- set-4/p3.py
+++ set-4/p3.py
@@ -1,1 +1,15 @@
 #3. Write Program to find missing elements of a range
+def find_missing_elements(arr,start,end):
+    full_range=set(range(start,end+1))
+    arr_set=set(arr)
+    missing_elements=aorted(list(full_range-arr_Set))
+    return missing_elements
+arr = [1, 2, 4, 5, 7, 9]
+start = 1
+end = 10
+
+missing = find_missing_elements(arr, start, end)
+print("Missing elements:", missing)  
+  
+  
+  
