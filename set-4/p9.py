57fc535a17403d3d95f2568f311c8bbaa4c336a7
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-08 07:01:43 +0530

    Update p9.py

--- set-4/p9.py
+++ set-4/p9.py
@@ -1,1 +1,11 @@
 #9. Write Program to find sum of elements in an array
+def find_sum(arr):
+    # Use the built-in sum() function to calculate the sum of the array
+    total_sum = sum(arr)
+    return total_sum
+
+# Example usage
+arr = [10, 20, 30, 40, 50]
+
+total = find_sum(arr)
+print("Sum of elements:", total)
