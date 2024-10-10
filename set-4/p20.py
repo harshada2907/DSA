505fe4dbf74d6b9e5b0bc2c28288b64d9c2757fa
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-10 20:54:30 +0530

    Update p20.py

--- set-4/p20.py
+++ set-4/p20.py
@@ -1,1 +1,15 @@
+#20.Write Program to find maximum scalar product of two vectors
+def max_scalar_product(vector1, vector2):
+    vector1.sort()
+    vector2.sort()
+    
+    scalar_product = sum(a * b for a, b in zip(vector1, vector2))
+    
+    return scalar_product
 
+
+vector1 = [1, 3, 5]
+vector2 = [2, 4, 6]
+
+result = max_scalar_product(vector1, vector2)
+print("Maximum scalar product:", result)
