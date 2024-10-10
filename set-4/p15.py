1ee7f45fb2518052d11e9781039b847d1d54ba0b
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-10 07:05:36 +0530

    Update p15.py

--- set-4/p15.py
+++ set-4/p15.py
@@ -1,1 +1,14 @@
 #15.Write Program to for sorting the elements of an array
+# Function to sort an array
+def sort_array(arr):
+    arr.sort()
+    return arr
+
+# Example array
+my_array = [34, 12, 5, 67, 1, 89, 23]
+
+# Sorting the array
+sorted_array = sort_array(my_array)
+
+# Display the sorted array
+print("Sorted Array:", sorted_array)
