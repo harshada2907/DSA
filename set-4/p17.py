dcb2d71da40d0f76c661c1920e3991d28ccd5a3c
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-10 20:50:29 +0530

    Update p17.py

--- set-4/p17.py
+++ set-4/p17.py
@@ -1,1 +1,20 @@
+#17.Write Program to find maximum product subarray in a given array
+def max_product_subarray(arr):
+    n = len(arr)
+    max_product = arr[0]
+    min_product = arr[0]
+    result = arr[0]
+    
+    for i in range(1, n):
+        if arr[i] < 0:
+            max_product, min_product = min_product, max_product
+        
+        max_product = max(arr[i], max_product * arr[i])
+        min_product = min(arr[i], min_product * arr[i])
+        
+        result = max(result, max_product)
+    
+    return result
 
+arr = [2, 3, -2, 4]
+print("Maximum product subarray:", max_product_subarray(arr))
