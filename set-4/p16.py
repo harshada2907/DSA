39b3d89240fcbb6052290970e4be0742d1492127
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-10 20:48:55 +0530

    Update p16.py

--- set-4/p16.py
+++ set-4/p16.py
@@ -1,1 +1,7 @@
-
+#16.Write Program for Reversing an array
+def reverse_array(arr):
+  return arr[::-1]
+arr=[1,2,3,4,5]
+reversed_arr=reverse_array(arr)
+print("Original Array:",arr)
+print("Reveresed Array:",reversed_arr)
