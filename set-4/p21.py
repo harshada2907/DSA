6290a3d462bc9e1a6855bb14a0f63fce1a096624
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-10 20:57:49 +0530

    Update p21.py

--- set-4/p21.py
+++ set-4/p21.py
@@ -1,1 +1,9 @@
-
+#21.Write Program to find whether the numbers of an array be made equal?
+def can_be_made_equal(arr):
+  return len(set(arr))==1
+arr=[4,4,4,4]
+if can_be_made_equal(arr):
+  print("All elements a=can be made equal.")
+else:
+ print("The elements cannot be made equal.")   
+  
