ea199b9209c9bee752dc234693da74d18ecf91d8
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-11 19:36:25 +0530

    Update p23.py

--- set-4/p23.py
+++ set-4/p23.py
@@ -1,1 +1,7 @@
-
+#23.Write Program to count distinct elements of an array
+def count_distinct_elements(arr):
+  distinct_elements=set(arr)
+  return len(distinct_elements)
+array=[1,2,2,3,4,4,5]
+distinct_count=count_distinct_elements(array)
+print("Number of distinct elements:",distinct_count)
