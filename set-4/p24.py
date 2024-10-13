e825ce1fe328b5832ff945586521e1cd7fbdaa8d
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-13 15:02:40 +0530

    Update p24.py

--- set-4/p24.py
+++ set-4/p24.py
@@ -1,1 +1,17 @@
 #24.Write Program to find non-repeating elements of an array
+def find_non_repeating_elements(arr):
+    element_count = {}
+    
+    for element in arr:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+    
+    non_repeating = [element for element in arr if element_count[element] == 1]
+    
+    return non_repeating
+
+arr = [4, 5, 4, 5, 6, 7, 8, 8, 9]
+non_repeating_elements = find_non_repeating_elements(arr)
+print("Non-repeating elements:", non_repeating_elements)
