3511b4164ea97e160dfdaeffff15aa2e2c213e30
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2024-10-13 15:03:18 +0530

    Update p25.py

--- set-4/p25.py
+++ set-4/p25.py
@@ -1,1 +1,17 @@
 #25.Write Program to find repeating elements in an array
+def find_repeating_elements(arr):
+    element_count = {}
+    
+    for element in arr:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+    
+    repeating = [element for element in arr if element_count[element] > 1]
+    
+    return list(set(repeating))
+
+arr = [4, 5, 4, 5, 6, 7, 8, 8, 9]
+repeating_elements = find_repeating_elements(arr)
+print("Repeating elements:", repeating_elements)
